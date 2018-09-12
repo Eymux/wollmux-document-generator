b87f00946d1e899e05f00bfc90540fefe2c58021
Eymux <eymux2009@gmail.com>
2018-09-12 14:28:36 +0200

    odf-service hinzugefügt.

--- rest-service/src/main/java/de/muenchen/wollmux_document_generator/rest_service/Main.java
+++ rest-service/src/main/java/de/muenchen/wollmux_document_generator/rest_service/Main.java
@@ -1,10 +1,13 @@
 package de.muenchen.wollmux_document_generator.rest_service;
 
 import org.jooby.Jooby;
+import org.jooby.apitool.ApiTool;
 
 public class Main extends Jooby {
     {
         get("/", () -> "Hello World!");
+
+        use(new ApiTool().swagger("/swagger").raml("/raml"));
     }
 
     public static void main(final String[] args) {
